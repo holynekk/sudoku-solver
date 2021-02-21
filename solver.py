a00ecf56c9013ad14439318c8a5fcd564e493b00
HolynekK <e238109@metu.edu.tr>
2021-02-21 18:27:15 +0300

    Changed a bit for gui

--- solver.py
+++ solver.py
@@ -1,17 +1,5 @@
-import time
-# The sudoku grid
-grid = [
-        [7,8,0,4,0,0,1,2,0],
-        [6,0,0,0,7,5,0,0,9],
-        [0,0,0,6,0,1,0,7,8],
-        [0,0,7,0,4,0,2,6,0],
-        [0,0,1,0,5,0,9,3,0],
-        [9,0,4,0,6,0,0,0,5],
-        [0,7,0,3,0,0,0,1,2],
-        [1,2,0,0,0,7,4,0,0],
-        [0,4,9,2,0,6,0,0,7]]
 
-def isPossible(y, x, n):
+def isPossible(y, x, n, grid):
     # Check whether n is already exist in that row.
     for i in range(9):
         if grid[y][i] == n:
@@ -30,22 +18,26 @@ def isPossible(y, x, n):
     # That number can be put in there.  
     return True
 
-def solve():
+
+def solve(grid):
     # Scanning grid for empty square.
     for y in range(9):
         for x in range(9):
             if grid[y][x] == 0:
                 for i in range(1, 10):
-                    if isPossible(y, x, i):
+                    if isPossible(y, x, i, grid):
                         grid[y][x] = i
-                        solve()
+                        solve(grid)
                         grid[y][x] = 0
-                return
-    print_grid()
+                return grid
+    if all_filled(grid):
+        print("AFTER CALCULATION: ")
+        print_grid(grid)
+    return grid
                     
 
 # To print grid into console
-def print_grid():
+def print_grid(grid):
     for i in range(9):
         if(i % 3 == 0 and i != 0):
             print("- - - - - - - - - - - -")
@@ -56,3 +48,11 @@ def print_grid():
                 print(str(grid[i][j]))
             else:
                 print(str(grid[i][j]) + " ", end="")
+    print('\n')
+
+
+def all_filled(grid):
+    for i in range(9):
+        if grid[i].count(0) > 0:
+            return False
+    return True
